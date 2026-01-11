7181c830321f0ce23a57e30e734dd0932ac2a7ba
mattnalexander <200349269+mattnalexander@users.noreply.github.com>
2026-01-11 14:39:16 -0500

    Update installation instructions in llm-i.sh

--- llm-i.sh
+++ llm-i.sh
@@ -5,7 +5,10 @@
 # to install the homebrew tools. 
 # Llama.cpp uses the metal and apple give access to homebrew to do this. Therefore, M series Macs are using the GPU and run that way by default. No additional configuration is needed. 
 
+#Step 1 - requires sudo , copy and paste to terminal
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+#Step 2 - copy and paste to terminal
 # Install Llama.cpp through homebrew , run llama-server -hf <user/llm-name-gguf> --port 8080 
 brew install llama.cpp && llama-server -hf LoveGlaze/Llama3.3-8B-Instruct-Thinking-Claude-4.5-Opus-High-Reasoning-Q4_K_M-GGUF --port 8080
 
