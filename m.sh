f02f49c6f2417014f1488c550cbd0e024af146d7
mattnalexander <200349269+mattnalexander@users.noreply.github.com>
2026-01-11 13:56:34 -0500

    Create m.sh
    installation scripts and instructions for local llm using llama.cpp on m series MacBooks.

---
+++ m.sh
@@ -0,0 +1,8 @@
+# Install Local LLM on Mac Silicon M Series 
+
+#Step 1
+# Start with installation of homebrew tools. This part cannot be script as it requires a terminal restart to take effect. You may copy and paste the command below 
+# to install the homebrew tools. 
+
+
+#Step 2
