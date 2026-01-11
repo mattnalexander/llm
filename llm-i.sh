bddd40cf3148c57ea7805d80d532b219d3336b32
mattnalexander <200349269+mattnalexander@users.noreply.github.com>
2026-01-11 14:37:43 -0500

    llm-i.sh

--- llm-i.sh
+++ llm-i.sh
@@ -6,13 +6,9 @@
 # Llama.cpp uses the metal and apple give access to homebrew to do this. Therefore, M series Macs are using the GPU and run that way by default. No additional configuration is needed. 
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install Llama.cpp through homebrew , run llama-server -hf <user/llm-name-gguf> --port 8080 
+brew install llama.cpp && llama-server -hf LoveGlaze/Llama3.3-8B-Instruct-Thinking-Claude-4.5-Opus-High-Reasoning-Q4_K_M-GGUF --port 8080
 
-#Step 2
-#!/bin/bash
-set -e
-
-# Install Llama.cpp
-brew install llama.cpp
-brew install cmake 
-# Launch llama-server with HF model - Included is a gguf to help you understand how it works and get you started. 
-llama-server -hf meta-llama/Llama-3.1-8B-Instruct --port 8080
+#Open Web Browser
+#Navigate to https://127.0.0.1:8080
+#Ask away
