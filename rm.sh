dbf32a52d17ea5af95e355d4bf0e367a1538e56c
mattnalexander <200349269+mattnalexander@users.noreply.github.com>
2026-01-12 14:54:34 -0500

    /
    Added a comment explaining the purpose of the script.

--- rm.sh
+++ rm.sh
@@ -1,4 +1,4 @@
-
+#Removes and uninstalls llama.cpp and associated model content / directories 
 rm /Users/matthew/Library/Caches/llama.cpp/*
 rm -rf /usr/local/Homebrew && 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)" 
